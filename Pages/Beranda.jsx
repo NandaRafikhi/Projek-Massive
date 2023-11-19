6447f4c2ed15918f2cf009d00574887ff6df65c6
Muhammad Rafidhia <93400873+tititsans@users.noreply.github.com>
2023-11-19 22:53:14 +0700

    Add files via upload

---
+++ Pages/Beranda.jsx
@@ -0,0 +1,16 @@
+import Navbar from '../Component/Navbar'
+import IdeaGroup from '../Component/IdeaCard/IdeaGroup'
+import ContohGroup from '../Component/Contoh/ContohGroup'
+
+
+const Beranda = () => {
+  return (
+    <>
+        <Navbar />
+        <IdeaGroup />
+        <ContohGroup /> 
+    </>
+  )
+}
+
+export default Beranda
