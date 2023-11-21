b626e2f9a37365c7c28c349781be48a80c2db63f
Muhammad Rafidhia <93400873+tititsans@users.noreply.github.com>
2023-11-21 23:26:20 +0700

    Add files via upload

--- front-end/src/Pages/Beranda.jsx
+++ front-end/src/Pages/Beranda.jsx
@@ -1,11 +1,13 @@
 import IdeaGroup from '../Component/IdeaCard/IdeaGroup'
 import Navbar from '../Component/Navbar'
+import ContohGroup from '../Component/Contoh/ContohGroup'
 
 const Beranda = () => {
   return (
     <>
         <Navbar />
         <IdeaGroup />
+        <ContohGroup />
     </>
   )
 }
