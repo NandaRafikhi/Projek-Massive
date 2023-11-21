260a7279c7b8436fe73eefcf5488958bec595bf7
Muhammad Rafidhia <93400873+tititsans@users.noreply.github.com>
2023-11-21 23:22:55 +0700

    Add files via upload

---
+++ front-end/front-end/src/Pages/Beranda.jsx
@@ -0,0 +1,15 @@
+import IdeaGroup from '../Component/IdeaCard/IdeaGroup'
+import Navbar from '../Component/Navbar'
+import ContohGroup from '../Component/Contoh/ContohGroup'
+
+const Beranda = () => {
+  return (
+    <>
+        <Navbar />
+        <IdeaGroup />
+        <ContohGroup />
+    </>
+  )
+}
+
+export default Beranda
