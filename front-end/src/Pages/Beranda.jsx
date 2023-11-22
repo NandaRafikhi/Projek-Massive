28b89f754df9e25cd54eb182deaf55df26c8bb7a
Enrico Airlangga <airlangga7ots@gmail.com>
2023-11-23 00:59:29 +0700

    Update carousel & footer

--- front-end/src/Pages/Beranda.jsx
+++ front-end/src/Pages/Beranda.jsx
@@ -1,15 +1,19 @@
-import IdeaGroup from '../Component/IdeaCard/IdeaGroup'
 import Navbar from '../Component/Navbar'
+import Carousel from '../Component/Carousel'
+import JenisProduk from '../Component/JenisProduk/JenisProduk'
+import IdeaGroup from '../Component/IdeaCard/IdeaGroup'
 import ContohGroup from '../Component/Contoh/ContohGroup'
-import ReviewGroup from '../Component/Review/ReviewGroup'
+import Footer from '../Component/Footer'
 
 const Beranda = () => {
   return (
     <>
         <Navbar />
+        <Carousel />
+        <JenisProduk />
         <IdeaGroup />
         <ContohGroup />
-        <ReviewGroup/>
+        <Footer />
     </>
   )
 }
