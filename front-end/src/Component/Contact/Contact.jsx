9f00f8f711b0ec9aa99b26a6e035728f5b2f6591
yo <92345964+ncayo@users.noreply.github.com>
2023-12-01 03:59:51 +0700

    img-contact

--- front-end/src/Component/Contact/Contact.jsx
+++ front-end/src/Component/Contact/Contact.jsx
@@ -7,7 +7,7 @@ function Contact() {
         <div className="w-full max-w-[1311px] mt-20 max-md:max-w-full max-md:mt-10">
           <div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
             <div className="flex flex-col items-stretch w-[37%] max-md:w-full max-md:ml-0">
-              <div className="pb-8 items-start flex grow flex-col px-3 max-md:max-w-full max-md:mt-10">
+              <div className="pb-8 pl-10 items-start flex grow flex-col px-3 max-md:max-w-full max-md:mt-10">
                 <div className="text-black text-center text-3xl font-mons font-bold self-stretch whitespace-nowrap max-md:max-w-full">
                   Hubungi kami
                 </div>
@@ -95,7 +95,7 @@ function Contact() {
               </div>
             </div>
             <div className="flex flex-col items-stretch w-[63%] ml-5 max-md:w-full max-md:ml-0">
-              {/* <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3987.1466397655918!2d99.83971307480532!3d2.096899297884353!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x302da1c77407d83b%3A0xd6f14efce2699d1!2sVisual%20Store%20Rantauprapat%20Sablon%20kaos!5e0!3m2!1sid!2sid!4v1701362981377!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" frameborder="0"></iframe> */}
+              <img src="../../public/assets/kontak/img-kontak.jpg" width="550" className="ml-32 mt-1" alt="" />
             </div>
           </div>
         </div>
