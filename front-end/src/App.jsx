769c37d62e6ac7dd329b1618daa3ac743ca30852
reyvli <145971858+reyvli@users.noreply.github.com>
2023-12-02 00:51:23 +0700

    Tentang ,carousel2

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,6 +1,7 @@
 import {BrowserRouter as Router,Routes,Route } from "react-router-dom"
 
 import Beranda from './Pages/Beranda'
+import Tentang from "./Pages/vstore/Tentang"
 import Masuk from "./Pages/vstore/Masuk"
 import Daftar from "./Pages/vstore/Daftar"
 
@@ -9,6 +10,7 @@ const App = () => {
     <Router>
       <Routes>
         <Route path="/" element={<Beranda />} />
+        <Route path="/vstore/tentang" element={<Tentang />} />
         <Route path="/vstore/masuk" element={<Masuk />} />
         <Route path="/vstore/daftar" element={<Daftar />} />
       </Routes>
