ad6cfbe78f6d8817d1d71b15d01d50528c05df52
reyvli <145971858+reyvli@users.noreply.github.com>
2023-12-05 02:49:16 +0700

    user profile

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -4,7 +4,8 @@ import Beranda from './Pages/Beranda'
 import Masuk from "./Pages/vstore/Masuk"
 import Daftar from "./Pages/vstore/Daftar"
 import Produk from "./Pages/vstore/Produk"
-
+import User from "./Pages/vstore/User";
+import Edit from "./Component/Edit/edit";
 const App = () => {
   return (
     <Router>
@@ -13,6 +14,8 @@ const App = () => {
         <Route path="/vstore/masuk" element={<Masuk />} />
         <Route path="/vstore/daftar" element={<Daftar />} />
         <Route path="/vstore/produk" element={<Produk />} />
+        <Route path="/vstore/User" element={<User />} />
+        <Route path="/vstore/Edit" element={<Edit />} />
       </Routes>
     </Router>
   )
---
+++ front-end/src/Pages/vstore/Edit.jsx
@@ -0,0 +1,16 @@
+import Footer from "../../Component/Footer/Footer";
+import Navbar from "../../Component/Navbar/Navbar";
+import Ganti from "../../Component/Edit/edit"
+
+
+function Edit () {
+    return(
+        <>
+            <Navbar/>
+            <Ganti/>
+            <Footer/>
+        </>
+    )
+}
+
+export default Edit;
