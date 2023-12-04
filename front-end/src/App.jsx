aae8427ece8e8ae811b6c774ac405590ba9ae409
Muhammad Rafidhia <93400873+tititsans@users.noreply.github.com>
2023-12-04 14:04:39 +0700

    Add files via upload

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,18 +1,18 @@
 import {BrowserRouter as Router,Routes,Route } from "react-router-dom"
 
 import Beranda from './Pages/Beranda'
-import Tentang from "./Pages/vstore/Tentang"
 import Masuk from "./Pages/vstore/Masuk"
 import Daftar from "./Pages/vstore/Daftar"
+import Produk from "./Pages/vstore/Produk"
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Beranda />} />
-        <Route path="/vstore/tentang" element={<Tentang />} />
         <Route path="/vstore/masuk" element={<Masuk />} />
         <Route path="/vstore/daftar" element={<Daftar />} />
+        <Route path="/vstore/produk" element={<Produk />} />
       </Routes>
     </Router>
   )
