7b796804f885737715be70ac6c79bd4a8d790a73
yo <92345964+ncayo@users.noreply.github.com>
2023-12-05 13:06:20 +0700

    app-jsx&delete

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -9,6 +9,7 @@ import Daftar from "./Pages/vstore/Daftar";
 import Desain from "./Pages/vstore/Desain";
 import User from "./Pages/vstore/User";
 import Edit from "./Pages/vstore/Edit";
+import Tampilan from "./Pages/vstore/Tampilan";
 
 const App = () => {
   return (
@@ -23,6 +24,7 @@ const App = () => {
         <Route path="/vstore/desain" element={<Desain />} />
         <Route path="/vstore/user" element={<User />} />
         <Route path="/vstore/edit" element={<Edit />} />
+        <Route path="/vstore/tampilan" element={<Tampilan/>}/>
       </Routes>
     </Router>
   );
--- front-end/src/Component/Component/Review/ReviewCard.jsx
+++
@@ -1,7 +0,0 @@
-export default function ReviewCard({imgSrc, ...props}) {
-    return (
-        <div {...props}
-        className="relative w-[296px] h-[180px] overflow-hidden rounded-2xl mx-4 shadow-md bg-soft-gray">
-    </div>
-    )
-}
